1cdd701e8a010c8356ad43510370176ee78469da
Darkphel <80648577+fernando527@users.noreply.github.com>
2024-08-11 22:34:35 -0600

    se agrego calendario

--- lib/principal_page.dart
+++ lib/principal_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class PrincipalPage extends StatefulWidget {
   const PrincipalPage({super.key});
@@ -24,7 +25,7 @@ class _PrincipalPageState extends State<PrincipalPage> {
           // },
           children: [
             const HomeMenuPage(),
-            Container(color: Color.fromARGB(255, 4, 230, 255)),
+            const CalendarioPage(),
             Container(color: Color.fromARGB(255, 4, 230, 255)),
           ],
         ),
@@ -69,6 +70,7 @@ class _PrincipalPageState extends State<PrincipalPage> {
   }
 }
 
+//pestaña del menu principal
 class HomeMenuPage extends StatelessWidget {
   const HomeMenuPage({
     super.key,
@@ -78,5 +80,74 @@ class HomeMenuPage extends StatelessWidget {
   Widget build(BuildContext context) {
     print("Home Menu Page cargado");
     return Container(color: Color.fromARGB(255, 4, 230, 255));
+  }
+}
+
+//pestaña de calendario
+class CalendarioPage extends StatefulWidget {
+  const CalendarioPage({super.key});
+
+  @override
+  _CalendarioPageState createState() => _CalendarioPageState();
+}
+
+class _CalendarioPageState extends State<CalendarioPage> {
+  final CalendarFormat _calendarFormat = CalendarFormat.month;
+
+  DateTime ahora = DateTime.now();
+  DateTime? _diaseleccionado;
+  Map<DateTime, List<dynamic>> eventos = {};
+
+  void _diaSeleccionado(DateTime dia, DateTime focusedDay) {
+    setState(() {
+      ahora = dia;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(height: 36),
+          TableCalendar(
+            headerStyle: const HeaderStyle(
+              titleCentered: true,
+              formatButtonVisible: false,
+            ),
+            availableGestures: AvailableGestures.all,
+            focusedDay: ahora,
+            selectedDayPredicate: (hoy) => isSameDay(hoy, ahora),
+            firstDay: DateTime.utc(2020, 1, 1),
+            lastDay: DateTime.utc(2030, 12, 31),
+            onDaySelected: _diaSeleccionado,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            calendarFormat: _calendarFormat,
+            calendarStyle: CalendarStyle(
+              selectedDecoration: BoxDecoration(
+                color: Colors.blueAccent.withOpacity(0.2),
+                shape: BoxShape.circle,
+              ),
+              selectedTextStyle: const TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+              todayDecoration: const BoxDecoration(
+                color: Colors.blueAccent,
+                shape: BoxShape.circle,
+              ),
+              todayTextStyle: const TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+              outsideDaysVisible: false,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
