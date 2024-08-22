0b338248dbf4d141f00b50d6a907e215fef6ba4a
Darkphel <80648577+fernando527@users.noreply.github.com>
2024-08-21 20:23:32 -0600

    diseño de inicio

--- lib/inicio.dart
+++ lib/inicio.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/RutinaPage.dart';
 import 'package:myapp/crearRutina.dart';
 import 'package:myapp/calendario.dart';
-//import 'package:myapp/DetalleRutinaPage.dart';
 
-//pestaña del menu principal
 class HomeMenuPage extends StatelessWidget {
   const HomeMenuPage({
     super.key,
@@ -14,7 +13,7 @@ class HomeMenuPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.pink[50],
       appBar: AppBar(
-        automaticallyImplyLeading: false, 
+        automaticallyImplyLeading: false,
         title: Text('FitLife', style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.pink[50],
         elevation: 0,
@@ -30,135 +29,173 @@ class HomeMenuPage extends StatelessWidget {
               ),
             ),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Crearrutina()),
-                      );
-                    },
-                    child: Container(
+          SingleChildScrollView(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Crearrutina()),
+                        );
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Cree su plan',
+                                      style: TextStyle(
+                                          fontSize: 30.0,
+                                          fontWeight: FontWeight.bold)),
+                                  SizedBox(height: 15.0),
+                                  Text(
+                                    'Crear',
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(16.0),
+                              child: Opacity(
+                                opacity: 0.7,
+                                child: Image.asset(
+                                  'assets/imagen8.png',
+                                  width: 150.0,
+                                  height: 150.0,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => CalendarioPage()),
+                              );
+                            },
+                            child: Container(
+                              padding: EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 204, 255, 204),
+                                borderRadius: BorderRadius.circular(16.0),
+                              ),
+                              child: Text('Calendario',
+                                  style: TextStyle(fontSize: 18.0)),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 16.0),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Crearrutina()),
+                              );
+                            },
+                            child: Container(
+                              padding: EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 204, 255, 204),
+                                borderRadius: BorderRadius.circular(16.0),
+                              ),
+                              child: Text('Mis Rutinas',
+                                  style: TextStyle(fontSize: 18.0)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16.0),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => RutinaPage()),
+                        );
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        height: 150.0,
+                        padding: EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.0),
+                          image: DecorationImage(
+                            image: AssetImage('assets/imagen7.png'),
+                            fit: BoxFit.cover,
+                            colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.3),
+                              BlendMode.darken,
+                            ),
+                          ),
+                        ),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Comenzar a entrenar',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    Container(
                       padding: EdgeInsets.all(16.0),
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(16.0),
                       ),
-                      child: const Row(
+                      child: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Cree su plan',
-                                    style: TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold)),
-                                SizedBox(height: 10.0),
-                                Text('Crear', style: TextStyle(fontSize: 16.0)),
-                              ],
-                            ),
-                          ),
+                          Text('Bienvenido de nuevo',
+                              style: TextStyle(fontSize: 18.0)),
+                          SizedBox(height: 8.0),
+                          Text('¿Quieres comenzar a entrenar ya?',
+                              style: TextStyle(fontSize: 16.0)),
+                          Text('¿Quieres crear otro plan?',
+                              style: TextStyle(fontSize: 16.0)),
+                          Text(
+                              'Si tu respuesta fue si ve a la pestaña de Rutinas y crea otra rutina',
+                              style: TextStyle(fontSize: 16.0)),
                         ],
                       ),
                     ),
-                  ),
-                  SizedBox(height: 16.0),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => CalendarioPage()),
-                      );
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child:
-                          Text('Calendario', style: TextStyle(fontSize: 18.0)),
-                    ),
-                  ),
-                  SizedBox(height: 16.0),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Crearrutina()),
-                            );
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(16.0),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(16.0),
-                            ),
-                            child: Text('Mis Rutinas',
-                                style: TextStyle(fontSize: 18.0)),
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 16.0),
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Crearrutina()),
-                            );
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(16.0),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(16.0),
-                            ),
-                            child: Text('Comenzar a Entrenar',
-                                style: TextStyle(fontSize: 18.0)),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16.0),
-                  Container(
-                    padding: EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                    child: const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Bienvenido de nuevo',
-                            style: TextStyle(fontSize: 18.0)),
-                        SizedBox(height: 8.0),
-                        Text('¿Quieres comenzar a entrenar ya?',
-                            style: TextStyle(fontSize: 16.0)),
-                        Text('¿Quieres crear otro plan?',
-                            style: TextStyle(fontSize: 16.0)),
-                        Text(
-                            'Si tu respuesta fue si ve a la pestaña de Rutinas y crea otra rutina',
-                            style: TextStyle(fontSize: 16.0)),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
